e5defcc013de049fe52fffcc7ff706f7858c68a3
ShadowStormOne <Admin@DESKTOP-VKG2DKT>
2025-08-05 16:11:49 +0500

    Added a font to the Resources file

--- resource.h
+++ resource.h
@@ -9,12 +9,13 @@
 #define IDI_ICON3                       112
 #define IDB_PNG1                        135
 #define IDB_PNG2                        136
+#define IDR_RCDATA1                     137
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_RESOURCE_VALUE        138
 #define _APS_NEXT_COMMAND_VALUE         40003
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
